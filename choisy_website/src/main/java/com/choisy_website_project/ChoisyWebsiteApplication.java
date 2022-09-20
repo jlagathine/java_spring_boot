1a7f5a59c4f30f59429a98b8be11832c5cd8f771
jlagathine <jagathine@conseil-etat.local>
2022-09-20 16:36:43 +0200

    nouveau projet

---
+++ choisy_website/src/main/java/com/choisy_website_project/ChoisyWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.choisy_website_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoisyWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoisyWebsiteApplication.class, args);
+	}
+
+}
---
+++ choisy_website/src/test/java/com/choisy_website_project/ChoisyWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choisy_website_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoisyWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
