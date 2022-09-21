927cdf95d8511f12926ce32328667ecc28ab8af1
jlagathine <jlagathine@gmail.com>
2022-09-21 18:46:43 +0200

    dernier enregistrement

--- website/src/main/java/com/choisy/website/WebsiteApplication.java
+++ website/src/main/java/com/choisy/website/WebsiteApplication.java
@@ -1,16 +1,23 @@
 package com.choisy.website;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.website.controller.pageController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsiteApplication.class, args);
-		pageController.homePage();
 	}
+		
+		@GetMapping
+		public List<String> homePage() {
+			return List.of("pages/home",  "Hello"); 
+		}
 
 }
