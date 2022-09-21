81975cbb715bb90e3114197e78ba64222733da48
jlagathine <jagathine@conseil-etat.local>
2022-09-21 10:15:14 +0200

    nouveau projet website

---
+++ website/src/main/java/com/choisy/website/WebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.choisy.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteApplication.class, args);
+	}
+
+}
