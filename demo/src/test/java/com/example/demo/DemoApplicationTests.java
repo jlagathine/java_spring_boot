66bc94df0467758626f5b166ca46ce6a13d580b3
jlagathine <jlagathine@gmail.com>
2022-09-22 17:51:11 +0200

    Mise a jour Demo

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.choisy.website;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebsiteApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
